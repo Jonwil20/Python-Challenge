9e2ee50d05240f3e64a7fe494ccb7459fe6a432f
Jonwil20 <51329302+Jonwil20@users.noreply.github.com>
2019-07-06 21:18:34 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,23 @@
 
+import os
+import csv
+
+budget_data = os.path.join("..", "Python_Stuff", "budget_data.csv")
+
+with open(budget_data, 'r') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter = '.') 
+        total_sum = sum(1 for line in open(budget_data)) - 1
+with open(budget_data, 'r') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter = '.')
+        total = sum(2 for line in open(budget_data)) - 1
+        
+        average = total/total_sum
+        
+        
+print ('Financial Analysis')
+print ('----------------------------')
+print ('Total Months:', total)
+print('Total:', total_sum)
+print ('Average Change:', average)
+print ('Greatest Increase in Profits: ', max(budget_data))
+print ('Greatest Decrease in Profits: ', min(budget_data))        
